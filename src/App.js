de8b2371ffb6216eb763d4d26fa1c0f8aa490c6d
unknown <akbarsayakov@gmail.com>
2021-12-27 17:21:36 -0800

    Creating Your First Component & Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
+import Header from './components/Header'
+
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    { id: 1, text: 'Good' },
-    { id: 2, text: 'Norm' },
-    { id: 3, text: 'Bad' },
-  ]
-
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      <div className='comments'>
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
+    <>
+      <Header text='Hello' />
+      <div className='container'>
+        <h1>My App</h1>
       </div>
-    </div>
+    </>
   )
 }
 
