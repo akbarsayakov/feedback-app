0311121dea87ba30928c001f82ead4948444c1a3
unknown <akbarsayakov@gmail.com>
2021-12-27 16:54:43 -0800

    Dynamic Values

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    { id: 1, text: 'Good' },
+    { id: 2, text: 'Norm' },
+    { id: 3, text: 'Bad' },
+  ]
+
   return (
     <div>
-      <h1>My App </h1>
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
